37dc2d18e58a9fbfcfcbd7fd3b6df2c8d3993410
afiatamanna06 <afiatamanna06@gmail.com>
2019-10-25 12:41:46 +0600

    logger js file updated

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ function myFunction(name) {
     console.log('Hello ' + name);
 }
 myFunction('Afia');
+console.log(module);
+var logger = require('./logger');
+console.log(logger);
+logger.log('message');
